0d12d716a0f5ccc53d22c1c2fbe83409e2d1c4b1
maritsandstad <marit.sandstad@astro.uio.no>
2024-10-04 10:06:20 +0200

    Reading in era5-data added

--- src/fscve/forest_data_handler.py
+++ src/fscve/forest_data_handler.py
@@ -5,6 +5,8 @@ import xarray as xr
 import pandas as pd
 import numpy as np
 
+from .get_data_from_era5land import get_data_for_lat_lon_vector
+
 LOGGER = logging.getLogger(__name__)
 
 def take_forest_dataset_and_convert_to_pd(filepath, variable_list):
@@ -25,6 +27,39 @@ def take_forest_dataset_and_convert_to_pd(filepath, variable_list):
         raise ValueError(
                 f"All variables in {variable_list} must be in the dataset, but here are only {df.columns}"    
             )       
-
-    # TODO, drop rows with zeros in variable list
+    df_to_evaluate = df[variable_list]
+    df_zeros = df_to_evaluate*0.0
+    diff = df_to_evaluate.compare(df_zeros)
+    df = df.loc[diff.index]
     return df
+
+def variable_mapping(variable, forest_columns):
+    if variable.upper() in forest_columns:
+        return True, variable.upper()
+    if variable.lower() in forest_columns:
+        return True, variable.lower()
+    return False, variable
+    
+def variable_mapping_era5(variable):
+    if variable.upper() in ["ELEVATION", "HGT"]:
+        return "HGT-ERA"
+    if variable.upper() in ["TAS", "TAVG"]:
+        return "T2M"
+    if variable.upper() in ["PR", "RSDS", "RLDS"]:
+        return f"{variable.upper()}-ERA"
+    return variable.upper()
+
+def add_variables_to_forest_dataset(filepath, full_variable_list, forest_variables):
+    df_forest = take_forest_dataset_and_convert_to_pd(filepath, forest_variables)
+    variables_missing = []
+    for variable in full_variable_list:
+        if variable not in df_forest.columns:
+            rename, to_rename = variable_mapping(variable, df_forest.columns)
+            if rename:
+                df_forest.rename(columns={to_rename:variable}, inplace=True)
+            else:
+                variables_missing.append(variable)
+    for variable in variables_missing:
+        df_forest[variable] = get_data_for_lat_lon_vector(variable_mapping_era5(variable), df_forest["LAT"].values, df_forest["LON"].values)
+
+    return df_forest
--- tests/unit/test_forest_data_handler.py
+++ tests/unit/test_forest_data_handler.py
@@ -13,4 +13,19 @@ def test_take_forest_dataset_and_convert_to_pd():
     print(test.head())
     print(test.index.nlevels)
     print(type(test))
-    assert False
+    assert set(test.columns) == set(["lat", "lon", "agb_era5land", "conifer_era5land"])
+    assert test.index.nlevels == 1
+    assert len(test.index) < 1e5
+
+
+def test_variable_mappings():
+    assert set(forest_data_handler.variable_mapping("lat", ["LAT", "LON"])) == set((True, "LAT"))
+    assert set(forest_data_handler.variable_mapping("LON", ["lat", "lon"])) == set((True, "lon")) 
+    assert forest_data_handler.variable_mapping_era5("elevation") == "HGT-ERA"
+    assert forest_data_handler.variable_mapping_era5("TAVG") == "T2M"
+    assert forest_data_handler.variable_mapping_era5("Hello") == "HELLO"
+    assert forest_data_handler.variable_mapping_era5("Rsds") == "RSDS-ERA"
+
+def test_add_variables_to_forest_dataset():
+    test = forest_data_handler.add_variables_to_forest_dataset("/div/no-backup-nac/users/masan/PATHFINDER/PathFinder_WP3_Task3.5_emulator/agb_and_confier_on_era5-land_resolution_test.nc", ["LAT", "LON", "ELEVATION", "TAVG", "agb_era5land", "conifer_era5land"], ["agb_era5land", "conifer_era5land"])
+    assert set(test.columns) == set(["LAT", "LON", "ELEVATION", "TAVG", "agb_era5land", "conifer_era5land"])
